01197ae2c96ba31514098515a416abd549e1ddd6
Devin Frank <devinfrank@Devins-iMac.local>
2025-10-20 09:28:37 -0400

    Add DATABASE_URL support for Render

--- config.py
+++ config.py
@@ -13,6 +13,7 @@ REQUEST_STATUS_CHOICES = ("new", "queued", "done", "declined")
 # Database config
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DEFAULT_SQLITE = "sqlite:///" + os.path.join(BASE_DIR, "musician.db")
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', DEFAULT_SQLITE)
 
 # Other configs
 SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
